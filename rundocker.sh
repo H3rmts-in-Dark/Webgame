30c55021ed3de84e921abc10f48f4bd13a968937
H3rmt <stemmer.enrico@gmail.com>
2021-10-06 22:04:16 +0200

    updated rundocker.sh

--- rundocker.sh
+++ rundocker.sh
@@ -1,2 +1,5 @@
+# only one time
+# docker build -t preparedimage -f PrepareDockerimage .
 docker build -t webgame .
-docker run --rm --name WebGame -p 9090:18265 webgame
+
+docker run --rm --name WebGame -p 18265 webgame
