d9b3b53d385a6e6b5e99047e208facf6a3133482
H3rmt <stemmer.enrico@gmail.com>
2021-12-19 02:10:16 +0100

    changed scripts a bit

--- scripts/createCerts.sh
+++ scripts/createCerts.sh
@@ -1,7 +1,6 @@
 openssl genpkey -algorithm RSA -out server.key
 #openssl genrsa -out server.key 4096
 openssl ecparam -genkey -name secp384r1 -out server.key
-openssl req -new -x509 -sha256 -key server.key -out server.pem -days 3650
-pwd
-mv server.pem ../server/server.pem
-mv server.key ../server/server.key
+yes '' | openssl req -new -x509 -sha256 -key server.key -out server.pem -days 3650
+mv server.pem server/server.pem
+mv server.key server/server.key
