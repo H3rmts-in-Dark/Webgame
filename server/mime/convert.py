006089c01ad51b6502d514b0385afd00269bb81d
H3rmt <stemmer.enrico@gmail.com>
2022-03-06 21:47:49 +0100

    changed mime generation to output regex

--- server/mime/convert.py
+++ server/mime/convert.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
         flipped = {}
         for m, l in data.items():
             for i in l:
-                flipped[i] = m
+                flipped[i.replace("*", ".*", 1) + "$"] = m
         with open(sys.argv[2], "w") as r:
+            r.write(f"TRUNCATE {sys.argv[3]};\n")
             r.writelines([f"INSERT INTO {sys.argv[3]} (extension, mimetype) VALUES ('{ext}', '{typ}');\n" for ext, typ in flipped.items()])
             print("generated SQL")
