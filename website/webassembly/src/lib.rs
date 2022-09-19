2e37337d0b2cb934272965415fe2f9efc31bea21
H3rmt <stemmer.enrico@gmail.com>
2022-09-20 00:51:33 +0200

    tested some dom manipulations from wasm

--- website/webassembly/src/lib.rs
+++ website/webassembly/src/lib.rs
@@ -1,17 +1,34 @@
-extern crate core;
-
 use wasm_bindgen::prelude::*;
-
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use web_sys::{Document, Element};
 
 #[wasm_bindgen]
-extern {
-	fn alert(s: &str);
+extern "C" {
+	#[wasm_bindgen(js_namespace = console)]
+	fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-	alert("Hello, Wasm!");
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+	use web_sys::console;
+
+	log(&format!("Hello from {}!", "rust"));
+
+	console::log_1(&"Hello using web-sys".into());
+
+	let js: JsValue = 4.into();
+	console::log_2(&"Logging arbitrary values looks like".into(), &js);
+
+	// Use `web_sys`'s global `window` function to get a handle on the global
+	// window object.
+	let window = web_sys::window().expect("no global `window` exists");
+	let document: Document = window.document().expect("should have a document on window");
+
+	// Manufacture the element we're going to append
+	let val = document.create_element("p")?;
+	val.set_inner_html("Hello from Rust!");
+
+	let f: Option<Element> = document.get_element_by_id("123");
+	f.unwrap().append_child(&val)?;
+
+	Ok(())
 }
