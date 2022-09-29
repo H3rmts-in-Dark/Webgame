ab1a79e420b63681b1e70ff73952708a5232a2b0
H3rmt <stemmer.enrico@gmail.com>
2022-09-30 01:00:24 +0200

    added error catching to wasm ts call
    Signed-off-by: H3rmt <stemmer.enrico@gmail.com>

--- website/src/ts/wasm.ts
+++ website/src/ts/wasm.ts
@@ -1,12 +1,25 @@
 import init from "../wasm";
+import {run as runWASM} from "../wasm";
 
 async function run() {
+	console.log("WASM loading")
+	console.time("Load")
+	await init()
+	console.log("WASM loaded")
+	console.timeEnd("Load")
+
 	console.group("WASM started")
 	console.time("WASM")
-	await init()
-	console.groupEnd()
-	console.log("WASM finished")
-	console.timeEnd("WASM")
+	try {
+		await runWASM()
+		console.groupEnd()
+	} catch(e) {
+		console.groupEnd()
+		console.error("WASM Error:", e)
+	} finally {
+		console.log("WASM finished")
+		console.timeEnd("WASM")
+	}
 }
 
 export {run}
